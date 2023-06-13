783a3b8292aa4c069cfefa1b77cf022775705ed1
dheeraj <dheeraj.alim@gmail.com>
2023-06-14 00:25:03 +0530

    Improved logic using Lower Bound Solution of BS

--- Binary Search/search-insert-position.py
+++ Binary Search/search-insert-position.py
@@ -1,44 +1,32 @@
 '''
 
 https://leetcode.com/problems/search-insert-position/
+
+Can be solved using Lower Bound of Binary Search
 '''
 
-def search_insert_position(arr, target):
+def search_insert_position(arr, n, x):
 
     start = 0
-    end = len(arr)-1
+    end = n-1
+    ans = n
 
-    while start < end:
-
+    while start <= end:
         middle = (start + end)//2
 
-        if arr[middle] == target:
-            return middle
+        if arr[middle] >= x:
+            ans = middle
+            end = middle-1
 
-        if arr[middle] < target:
+        else:
             start = middle + 1
 
-        elif arr[middle] > target:
-            end = middle -1
+    return ans
 
 
-    # reaching heremeans target is missing, so we need to find its possibel index
+arr = [1,2,4,7]
+n = len(arr)
+x = 2
 
-    if arr[start] == target:
-        return start
-
-    if arr[start] > target:
-        if start == 0:
-            return 0
-
-        return start
-    else:
-        return start + 1
-
-
-arr = [1,3,5,6]
-arr = [1,3]
-target = 2
-
-print(search_insert_position(arr,target))
+print(search_insert_position(arr,n, x))
 
