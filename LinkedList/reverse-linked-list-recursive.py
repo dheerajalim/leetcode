4894d033fd2bb95e9ab497f30395ace4b6c88278
dheeraj <dheeraj.alim@gmail.com>
2023-12-11 13:26:09 +0530

    dec102023

--- LinkedList/reverse-linked-list-recursive.py
+++ LinkedList/reverse-linked-list-recursive.py
@@ -1,20 +1,18 @@
 from linkedlist import *
 
-
 ''' Solution 1 : The idea is to reach the last node of the linked list using recursion then start
  reversing the linked list.'''
 
 
 def reverse_recursive(head):
-
     if head is None or head.next is None:
         return head
 
-    rest_head = reverse_recursive(head.next)
+    rest_head = reverse_recursive(head.next)  # this is the last node that will be new head
 
-    rest_tail = head.next
-    rest_tail.next = head
-    head.next = None
+    rest_tail = head.next  # this is the node next to current head position as we need this to reverse the link
+    rest_tail.next = head  # here we reverse the link by making rest_tail.next to head of current recursive call
+    head.next = None  # this is the last node of the current recursive call, hence it's next must point to None
 
     return rest_head
 
@@ -23,15 +21,14 @@ def reverse_recursive(head):
 the remaining list.'''
 
 
-def reverse_recursive_2(curr, prev = None):
+def reverse_recursive_2(curr, prev=None):
+    if curr is None:  # base condition to understand we have reached the end of LL , i.e. passed the last node
+        return prev  # return the last node of the LL( means head in case of reversed LL)
 
-    if curr is None:
-        return prev
+    next = curr.next  # get the next node from curr, which will be new curr for the recursion
+    curr.next = prev  # the prev now moves to next node which is curr
 
-    next = curr.next
-    curr.next = prev
-
-    return reverse_recursive_2(next, curr)
+    return reverse_recursive_2(next, curr)  # call the recursion with new curr and prev
 
 
 arr = [10, 20, 30, 40, 50]
